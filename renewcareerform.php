81c9b52a3f9b58cb998642ab67f10b013458f658
Nick Skye <nickskye7@gmail.com>
2018-07-19 10:34:41 -0700

    Update renewcareerform.php

--- renewcareerform.php
+++ renewcareerform.php
@@ -1,49 +1,49 @@
-<?php 
-// $captcha;
-// if (isset($_POST['g-recaptcha-response'])) {
-//     $captcha = $_POST['g-recaptcha-response'];
-// }
-
-
-// Checking For correct reCAPTCHA
-// $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=SECRETKEY&response=" . $captcha . "&remoteip=" . $_SERVER['REMOTE_ADDR']);
-// if (!$captcha || $response.success == false) {
-//     echo "Your CAPTCHA response was wrong.";
-//     exit ;
-// } else {
-
-    $to = "nick@wbrandstudio.com"; // this is your Email address 
-    $from = $_POST['email']; // this is the sender's Email address
-    $fname = $_POST['fname'];
-    $lname = $_POST['lname'];
-    $phone = $_POST['phone'];
-
-    $subject = "Renew Careers Application";
-    
-    // $message = $fname . " " . $lname . "s info: " . "\n\n" . $from . "\n\n Address: \n" . $_POST['addressone'] . "\n\n" . $_POST['addresstwo'] ."\n\n" . $_POST['city'] . "\n\n" . $_POST['state'] . "\n\n" . $_POST['zip'] ."\n\n" . $_POST['country'] ."\n\n" . "\n\n Make: \n" . $_POST['make'] . "\n\n Model: \n" . $_POST['model'] . "\n\n Serial: \n" . $_POST['serial'] . "\n\n Age: \n" . $_POST['age'] . "\n\n Type: \n" . $_POST['upright'] . $_POST['grand'] . "\n\n Player: \n" . $_POST['player'] . $_POST['noplayer'] .$_POST['comment'];
-
-    $message = "<html><body>";
-$message .= "<table rules='all' style='border-color: #666;' cellpadding='10'>";
-$message .= "<tr style='background: #eee;'><td><strong>First Name:</strong> </td><td>" . $fname  . "</td></tr>";
-$message .= "<tr><td><strong>Last Name:</strong> </td><td>" . $lname . "</td></tr>";
-$message .= "<tr><td><strong>Email:</strong> </td><td>" . $from . "</td></tr>";
-$message .= "<tr><td><strong>Phone:</strong> </td><td>" . $phone . "</td></tr>";
-
-$message .= "<tr><td><strong>Comment:</strong> </td><td>" . strip_tags($_POST['comment']) . "</td></tr>";
-
-$message .= "</table>";
-$message .= "</body></html>";
-
-    $headers = "From: nick@wbrandstudio.com" ;
-    $headers .= "\r\n";
-$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-    $headers2 = "From:" . $to;
-   // echo "Mail Sent. Thank you " . $fname . ", we will contact you shortly.";
-    mail($to,$subject,$message,$headers);
-    //mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    header("Location: http://renewlandscapes.com/"); /* Redirect browser */
-exit();
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    // You cannot use header and echo together. It's one or the other.
- // }
+<?php
+require 'PHPMailer/PHPMailerAutoload.php';
+// require_once 'PHPMailer/class.phpmailer.php'
+$firstname = $_REQUEST['first-name'] ;
+$lastname = $_REQUEST['last-name'] ;
+$email = $_REQUEST['email'] ;
+$message = $_REQUEST['message'] ;
+if (
+    (strlen($firstname) <= 0)
+    || (strlen($lastname) <= 0)
+    || (strlen($email) <= 0)
+    || !filter_var($email, FILTER_VALIDATE_EMAIL)
+    || (strlen($message) <= 0)) {
+    echo 'All fields are required.';
+} else {
+    $mail = new PHPMailer;
+    // $mail->SMTPDebug = 3;                               // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'localhost';                            // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'no-reply@renewlandscapes.com';                 // SMTP username
+    $mail->Password = 'wollner123';                           // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
+    $mail->setFrom('no-reply@renewlandscapes.com', 'Mailer');
+    $mail->addAddress('digdeeper@renewlandscapes.com', 'Shelby');
+    $mail->addAddress('no-reply@renewlandscapes.com');     // Add 
+    $mail->addCC('nick@wbrandstudio.com');
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Contact submission from site';
+    $mail->Body    = '
+<html>
+<body>
+<h4>Name: '.$firstname.' '.$lastname.'</h3>
+<br>
+<h4>Email: '.$email.'</h3>
+<br>
+<p><strong>Message: </strong>'.$message.'</p>
+</body>
+</html>';
+    if(!$mail->send()) {
+        echo 'Message could not be sent.';
+        echo 'Mailer Error: ' . $mail->ErrorInfo;
+    } else {
+        header("location: about.html");
+        exit;
+    }
+}
 ?>
